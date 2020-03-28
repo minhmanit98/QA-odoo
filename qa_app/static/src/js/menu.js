730d56d26e17ffe456da541ca8d7768f1b0bfd30
Nguyen Minh Man <minhmanit98@gmail.com>
2020-03-29 00:18:01 +0700

    UTC2-9: Fix top menu
    + Fix top menu

---
+++ qa_app/static/src/js/menu.js
@@ -0,0 +1,139 @@
+odoo.define('qa_app.uct2_menu', function (require) {
+'use strict';
+
+var dom = require('web.dom');
+var publicWidget = require('web.public.widget');
+var wUtils = require('website.utils');
+
+publicWidget.registry.affixMenu = publicWidget.Widget.extend({
+
+});
+
+/**
+ * Auto adapt the header layout so that elements are not wrapped on a new line.
+ *
+ * Note: this works well with the affixMenu... by chance (autohideMenu is called
+ * after alphabetically).
+ */
+publicWidget.registry.autohideMenu = publicWidget.Widget.extend({
+    selector: 'header #top_menu',
+
+    /**
+     * @override
+     */
+    start: function () {
+        var self = this;
+        var defs = [this._super.apply(this, arguments)];
+        this.noAutohide = this.$el.closest('.o_no_autohide_menu').length;
+        if (!this.noAutohide) {
+            var $navbar = this.$el.closest('.navbar');
+            defs.push(wUtils.onceAllImagesLoaded($navbar));
+
+            // The previous code will make sure we wait for images to be fully
+            // loaded before initializing the auto more menu. But in some cases,
+            // it is not enough, we also have to wait for fonts or even extra
+            // scripts. Those will have no impact on the feature in most cases
+            // though, so we will only update the auto more menu at that time,
+            // no wait for it to initialize the feature.
+            var $window = $(window);
+            $window.on('load.autohideMenu', function () {
+                $window.trigger('resize');
+            });
+        }
+        return Promise.all(defs).then(function () {
+            if (!self.noAutohide) {
+                dom.initAutoMoreMenu(self.$el, {unfoldable: '.divider, .divider ~ li'});
+            }
+            self.$el.removeClass('o_menu_loading');
+        });
+    },
+    /**
+     * @override
+     */
+    destroy: function () {
+        this._super.apply(this, arguments);
+        if (!this.noAutohide) {
+            $(window).off('.autohideMenu');
+            dom.destroyAutoMoreMenu(this.$el);
+        }
+    },
+});
+
+/**
+ * Note: this works well with the affixMenu... by chance (menuDirection is
+ * called after alphabetically).
+ *
+ * @todo check bootstrap v4: maybe handled automatically now ?
+ */
+publicWidget.registry.menuDirection = publicWidget.Widget.extend({
+    selector: 'header .navbar .nav',
+    events: {
+        'show.bs.dropdown': '_onDropdownShow',
+    },
+
+    /**
+     * @override
+     */
+    start: function () {
+        this.defaultAlignment = this.$el.is('.ml-auto, .ml-auto ~ *') ? 'right' : 'left';
+        return this._super.apply(this, arguments);
+    },
+
+    //--------------------------------------------------------------------------
+    // Private
+    //--------------------------------------------------------------------------
+
+    /**
+     * @private
+     * @param {string} alignment - either 'left' or 'right'
+     * @param {integer} liOffset
+     * @param {integer} liWidth
+     * @param {integer} menuWidth
+     * @returns {boolean}
+     */
+    _checkOpening: function (alignment, liOffset, liWidth, menuWidth, windowWidth) {
+        if (alignment === 'left') {
+            // Check if ok to open the dropdown to the right (no window overflow)
+            return (liOffset + menuWidth <= windowWidth);
+        } else {
+            // Check if ok to open the dropdown to the left (no window overflow)
+            return (liOffset + liWidth - menuWidth >= 0);
+        }
+    },
+
+    //--------------------------------------------------------------------------
+    // Handlers
+    //--------------------------------------------------------------------------
+
+    /**
+     * @private
+     */
+    _onDropdownShow: function (ev) {
+        var $li = $(ev.target);
+        var $menu = $li.children('.dropdown-menu');
+        var liOffset = $li.offset().left;
+        var liWidth = $li.outerWidth();
+        var menuWidth = $menu.outerWidth();
+        var windowWidth = $(window).outerWidth();
+
+        $menu.removeClass('dropdown-menu-left dropdown-menu-right');
+
+        var alignment = this.defaultAlignment;
+        if ($li.nextAll(':visible').length === 0) {
+            // The dropdown is the last menu item, open to the left
+            alignment = 'right';
+        }
+
+        // If can't open in the current direction because it would overflow the
+        // window, change the direction. But if the other direction would do the
+        // same, change back the direction.
+        for (var i = 0 ; i < 2 ; i++) {
+            if (!this._checkOpening(alignment, liOffset, liWidth, menuWidth, windowWidth)) {
+                alignment = (alignment === 'left' ? 'right' : 'left');
+            }
+        }
+
+        $menu.addClass('dropdown-menu-' + alignment);
+    },
+});
+});
