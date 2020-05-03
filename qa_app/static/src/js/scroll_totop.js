674ca2941ab1ba620895afcfa42e00c4d3b42db5
Nguyen Minh Man <minhmanit98@gmail.com>
2020-05-04 00:22:54 +0700

    UTC2-29: Add button back-to-top
    Add button back-to-top

--- qa_app/static/src/js/scroll_totop.js
+++ qa_app/static/src/js/scroll_totop.js
@@ -1,20 +1,32 @@
-odoo.define('webtop_buttonadd_medicines.scroll_totop', function (require) {
+odoo.define('qa_app.scroll_totop', function (require) {
 'use strict';
 
 	var core = require('web.core');
 	var _t = core._t;
+	var QWeb = core.qweb;
 
 	window.onscroll = function() {scrollFunction()};
 
 	function scrollFunction() {
 	    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-	        document.getElementById("myBtn").style.display = "block";
+	    	$('#back-to-top').fadeIn();
+	        // document.getElementById("back-to-top").style.display = "block";
 	    } else {
-	        document.getElementById("myBtn").style.display = "none";
+	    	$('#back-to-top').fadeOut();
+	        // document.getElementById("back-to-top").style.display = "none";
 	    }
 	}
 	function topFunction() {
-	    document.body.scrollTop = 0;
-	    document.documentElement.scrollTop = 0;
+	    $('body,html').animate({
+				scrollTop: 0
+			}, 400);
+			return false;
 	}
+	$('#back-to-top').click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 400);
+			return false;
+	});
+
 });
