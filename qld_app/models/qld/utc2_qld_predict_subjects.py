5ef8bc1134c049d35b680c9670f35f6398356a13
Nguyen Minh Man <minhmanit98@gmail.com>
2020-06-20 02:07:36 +0700

    UTC2-43: complete Predict Scores
    complete Predict Scores

--- qld_app/models/qld/utc2_qld_predict_subjects.py
+++ qld_app/models/qld/utc2_qld_predict_subjects.py
@@ -28,6 +28,7 @@ class QLDPredictSubjects(models.Model):
         elif self.predict_subject1 and self.predict_subject2:
             return x1 * self.predict_a + x2 * self.predict_b + self.predict_c
 
+    @api.depends('subject_id', 'predict_subject1', 'predict_subject2')
     def _compute_predict(self):
         for record in self:
             if record.predict_subject1 and not record.predict_subject2:
@@ -70,13 +71,47 @@ class QLDPredictSubjects(models.Model):
                 record.predict_b = est.params[1]
                 record.predict_c = est.params[2]
 
-    name = fields.Char('Mã môn', default='New', readonly=True)
-    subject_id = fields.Many2one('utc2.qld.subjects', string='Môn dự đoán')
-    student_id = fields.Many2one('utc2.qld.students', string='Mã sinh viên')
+    @api.depends('subject_id', 'predict_subject1', 'predict_subject2')
+    def _compute_name(self):
+        for record in self:
+            if record.predict_subject1 and not record.predict_subject2:
+                record.name_display = str(record.subject_id.name) + '<-' + str(record.predict_subject1.name)
+            if record.predict_subject1 and record.predict_subject2:
+                record.name_display = str(record.subject_id.name) + '<-' + str(record.predict_subject1.name) + \
+                              ' + '+str(record.predict_subject2.name)
+
+    @api.depends('subject_id', 'predict_subject1', 'predict_subject2')
+    def _compute_name_display(self):
+        for record in self:
+            if record.predict_subject1 and not record.predict_subject2:
+                record.name = str(record.subject_id.name_display) + '<-' + str(record.predict_subject1.name_display)
+            if record.predict_subject1 and record.predict_subject2:
+                record.name = str(record.subject_id.name_display) + '<-' + str(record.predict_subject1.name_display) + \
+                              ' + '+str(record.predict_subject2.name_display)
+
+    @api.depends('name')
+    def _default_subject_id(self):
+        if self.env.context.get('subject_id'):
+            return self.env.context.get('subject_id')
+        else:
+            return False
+
+    name = fields.Char('Mã môn', compute=_compute_name_display, store=True, readonly=True, help="SS")
+    name_display = fields.Char('Mã môn', compute=_compute_name, store=True, readonly=True)
+    subject_id = fields.Many2one('utc2.qld.subjects', string='Môn dự đoán', required=True, default=_default_subject_id)
     state = fields.Selection([('current', 'Học như hiện tại'), ('try_hard', 'Cố gắng hơn')], 'Chế độ')
     predict_subject1 = fields.Many2one('utc2.qld.subjects', string='Môn liên quan 1', ondelete='restrict')
     predict_subject2 = fields.Many2one('utc2.qld.subjects', string='Môn liên quan 2', ondelete='restrict')
     class_id = fields.Many2one('utc2.qld.class', string='Lớp')
-    predict_a = fields.Float('Predict A', compute=_compute_predict, store=True)
-    predict_b = fields.Float('Predict B', compute=_compute_predict, store=True)
-    predict_c = fields.Float('Predict C', compute=_compute_predict, store=True)
+    group_id = fields.Many2one('utc2.qld.group', related="subject_id.group_id", string='Nhóm môn')
+    predict_a = fields.Float('Predict A', compute=_compute_predict, store=True, readonly=True)
+    predict_b = fields.Float('Predict B', compute=_compute_predict, store=True, readonly=True)
+    predict_c = fields.Float('Predict C', compute=_compute_predict, store=True, readonly=True)
+    predict_score_ids = fields.One2many('utc2.qld.predict.scores', 'predict_subject_id', string='Điểm đã tiên đoán')
+
+    @api.model
+    def create(self, vals):
+        if self.env.context.get('subject_id'):
+            vals['subject_id'] = self.env.context.get('subject_id')
+        result = super(QLDPredictSubjects, self).create(vals)
+        return result
